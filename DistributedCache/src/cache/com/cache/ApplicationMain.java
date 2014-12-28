089f7e5f79a63c7689ec7be0fe7719d80997a509
soumyajyotiask <soumyajyotiask@gmail.com>
2014-12-28 23:48:47 +0530

    DistributedCache
    Project committed and under construction

---
+++ DistributedCache/src/cache/com/cache/ApplicationMain.java
@@ -0,0 +1,35 @@
+package com.cache;
+
+import java.util.Map;
+import java.util.Queue;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class ApplicationMain {
+    public static void main(String[] args) {
+    	
+    	ApplicationContext applicationContext = new ClassPathXmlApplicationContext(args[0]);
+        /*Config cfg = new Config();
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
+        Map<Integer, String> mapCustomers = instance.getMap("customers");
+        mapCustomers.put(1, "Joe");
+        mapCustomers.put(2, "Ali");
+        mapCustomers.put(3, "Avi");
+ 
+        System.out.println("Customer with key 1: "+ mapCustomers.get(1));
+        System.out.println("Map Size:" + mapCustomers.size());
+ 
+        Queue<String> queueCustomers = instance.getQueue("customers");
+        queueCustomers.offer("Tom");
+        queueCustomers.offer("Mary");
+        queueCustomers.offer("Jane");
+        System.out.println("First customer: " + queueCustomers.poll());
+        System.out.println("Second customer: "+ queueCustomers.peek());
+        System.out.println("Queue size: " + queueCustomers.size());*/
+    }
+}
